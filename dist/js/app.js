7805a26d5f526f553cb9b31de610cf2d8d65fae3
Giorgos kalmoukis <giorgoskalmoukis@outlook.com>
2020-03-27 16:42:38 +0200

    Add js functions.

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,3 @@
-import { ConversationList } from "twilio/lib/rest/conversations/v1/conversation";
-
 const form = document.querySelector('form');
   form.addEventListener('submit', async event => {
     event.preventDefault();
@@ -17,8 +15,7 @@ const form = document.querySelector('form');
         // parse and submit all included form data
         body: new URLSearchParams(new FormData(form)).toString()
       });
-
-      console.log(form);
+      
       // if it was successful show success message
       if (response.status === 200) {
         document.querySelector('.when-success').hidden = false;
@@ -28,15 +25,4 @@ const form = document.querySelector('form');
     } catch (e) {
       console.error(e);
     }
-  });
-
-
-  document.addEventListener('DOMContentLoaded', () => {
-    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-      $notification = $delete.parentNode;
-  
-      $delete.addEventListener('click', () => {
-        $notification.parentNode.removeChild($notification);
-      });
-    });
   });
